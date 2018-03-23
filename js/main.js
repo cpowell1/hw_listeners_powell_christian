c473a4fc5c0a4cff173b6a5db7c0d4cb072d667a
Christian Powell <christiant.powell@gmail.com>
2018-03-22 19:13:13 -0500

    Update Event Listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var heading1 = document.getElementById('heading1');
+var clickNumber = 1;
+
 heading1.addEventListener('click', function () {
     var clickp = document.createElement('p');
-    clickp.innerHTML = '<p> This is click number ' + i + '</p>';
+    
+    
+    clickp.innerHTML = 'This is click number' + clickNumber;
+    clickNumber = clickNumber + 1;
+    
     document.getElementById('container').appendChild(clickp);
+    
+    
 });
