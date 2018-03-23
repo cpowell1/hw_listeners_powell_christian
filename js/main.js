da43e30a4bfaee093b7e61c02cb2786ae797c6c3
Christian Powell <christiant.powell@gmail.com>
2018-03-22 19:25:53 -0500

    Update Event Listener

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var formInfo = document.getElementById('form');
-formInfo.addEventListener('submit', function userInfo() {
-    userInfo.preventDefault();
-    var inputName = document.getElementsByClassName('inputValues');
-    console.log(inputName.value);
+var firstName = document.getElementById('inputValue1');
+var lastName = document.getElementById('inputValue2');
+var emailName = document.getElementById('inputValue3');
+var message = document.getElementById('inputValue4');
+
+formInfo.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('The first name is' + firstName.value);
+    console.log('The last name is' + lastName.value);
+    console.log('The email is' + emailName.value);
+    console.log('The message is' + message.value);
 });
 
